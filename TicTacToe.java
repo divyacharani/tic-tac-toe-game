fb166ec7c8723a5e697111ffe8c824d513d233e8
unknown <divyacharaninabha@gmail.com>
2020-10-03 20:17:49 +0530

    Added: Method to choose a letter for player added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+	static Scanner sc = new Scanner(System.in);
+	private static char userLetter;
+	private static char computerLetter;
 
 	public static void main(String[] args) {
 		// Welcome Message
 		System.out.println("Welcome to Tic Tac Toe game");
 		char board[] = createBoard();
+		userLetter = chooseLetter();
+		computerLetter = (userLetter == 'X') ? 'O' : 'X';
 	}
 
 	// Method to create board
@@ -15,4 +22,19 @@ public class TicTacToe {
 			board[index] = ' ';
 		return board;
 	}
+
+	// Method to choose a letter
+	private static char chooseLetter() {
+		char letter = ' ';
+		while (true) {
+			System.out.println("Choose a letter 'X' or 'O' ");
+			letter = sc.next().toUpperCase().charAt(0);
+			if (letter == 'X' || letter == 'O')
+				break;
+			else
+				System.out.println("Invalid Entry!! Choose a letter 'X or 'O'");
+		}
+		return letter;
+	}
+
 }
